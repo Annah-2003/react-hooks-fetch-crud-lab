5c03cade63f9eed1fb2f5cace51d58f799af0f0c
Irene Gitau <irene.gitau@strathmore.edu>
2023-10-30 23:14:14 +0300

    done

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import QuestionList from './components/QuestionList'; // Import QuestionList
 
-function QuestionList() {
+function App() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+      .then((response) => response.json())
+      .then((data) => setQuestions(data))
+      .catch((error) => console.error('Error fetching questions:', error));
+  }, []);
+
   return (
-    <section>
-      <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
+    <div>
+      <QuestionList questions={questions} /> {/* Use QuestionList component */}
+      {/* Render other components */}
+    </div>
   );
 }
 
-export default QuestionList;
+export default App;
